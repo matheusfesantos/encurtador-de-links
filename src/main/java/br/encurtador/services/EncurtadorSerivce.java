15d01eb3aee977b7a1ae73826b1086b1cbc7d675
matheusfesantos <matheusfgsantos31@gmail.com>
2025-03-06 17:16:03 -0300

    Adicionando Service para alterar o UUID do link e encurtar a URL

---
+++ src/main/java/br/encurtador/services/EncurtadorSerivce.java
@@ -0,0 +1,29 @@
+package br.encurtador.services;
+
+import br.encurtador.entity.urlEncurtada;
+import br.encurtador.repository.URLEncurtadaRepos;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class EncurtadorSerivce
+{
+    private URLEncurtadaRepos urlEncurtadaRepos;
+
+    public EncurtadorSerivce(URLEncurtadaRepos urlEncurtadaRepos){
+        this.urlEncurtadaRepos=urlEncurtadaRepos;
+    }
+
+    public String encurtarURL(String urlOriginal){
+        String urlCurta = UUID.randomUUID().toString().substring(0,8);
+        urlEncurtada urlEncurtada = new urlEncurtada(urlOriginal,urlCurta);
+        urlEncurtadaRepos.save(urlEncurtada);
+        return urlCurta;
+    }
+
+    public Optional<String> obterURLOriginal(String urlCurta){
+        return urlEncurtadaRepos.findCodigoCurto(urlCurta).map(urlEncurtada::getUrlOriginal);
+    }
+}
